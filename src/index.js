62187eb98f691f22c0eee00c10c857469951428f
Meenu <meenukatariya2330@gmail.com>
2022-06-17 20:10:27 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import { ChakraProvider } from '@chakra-ui/react'
+import { AuthContextProvider } from './context/AuthContext';
+
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <StrictMode>
-    <ColorModeScript />
+  <ChakraProvider>
+    <AuthContextProvider>
     <App />
-  </StrictMode>
+    </AuthContextProvider>
+  </ChakraProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/P
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
